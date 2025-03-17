13e8f4e505e1db2d30eeef6bb33207005a97aec5
Cristian <cristian.setto@gmail.com>
2025-03-17 00:04:12 -0300

    fix act seed env

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,42 +1,48 @@
-  import { PrismaClient } from "@prisma/client";
-  import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import 'dotenv/config';
 
-  const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-  async function main() {
-    const hashedPassword = await bcrypt.hash("admin123", 10);
-    
-    await prisma.user.upsert({
-      where: { email: 'admin@example.com' },
-      update: {
-        nombre: "Admin",
-        apellido1: "Ejemplo",
-        apellido2: "Test",
-        password: hashedPassword,
-        role: "admin",
-        rut: "111111111", // Valor válido para el RUT
-        createdAt: new Date(), // Fecha válida para evitar nulos
-      },
-      create: {
-        email: "admin@example.com",
-        nombre: "Admin",
-        apellido1: "Ejemplo",
-        apellido2: "Test",
-        password: hashedPassword,
-        role: "admin",
-        rut: "111111111", // Valor válido
-        createdAt: new Date(), // Fecha válida
-      },
-    });
+async function main() {
+  const adminPassword = process.env.ADMIN_PASSWORD; // Obtener la contraseña de la variable de entorno
+
+  if (!adminPassword) {
+    console.error("Error: ADMIN_PASSWORD environment variable is not set.");
+    process.exit(1);
   }
 
-  main()
-    .catch((e) => {
-      console.error(e);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+  const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
 
-    //Función: Crea un usuario admin al ejecutar npx prisma db seed.
+  await prisma.user.upsert({
+    where: { email: 'admin@example.com' },
+    update: {
+      nombre: "Admin",
+      apellido1: "Ejemplo",
+      apellido2: "Test",
+      password: hashedPassword,
+      role: "admin",
+      rut: "111111111",
+      createdAt: new Date(),
+    },
+    create: {
+      email: "admin@example.com",
+      nombre: "Admin",
+      apellido1: "Ejemplo",
+      apellido2: "Test",
+      password: hashedPassword,
+      role: "admin",
+      rut: "111111111",
+      createdAt: new Date(),
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

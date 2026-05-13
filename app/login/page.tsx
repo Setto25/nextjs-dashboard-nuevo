95384cb34f70f5682f02d40e8bfdf9c28c7de844
Setto25 <87681649+Setto25@users.noreply.github.com>
2026-05-12 21:32:42 -0400

    Actualizar page.tsx
    cambios en Github app

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -91,7 +91,7 @@ export default function LoginPage() {
                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                   </svg>
-                  Iniciando Servidor...
+                  Iniciando...
                 </>
               ) : (
                 "Ingresar al Sistema"
@@ -99,7 +99,7 @@ export default function LoginPage() {
             </button>
             {isLoading && (
               <p className="text-center text-xs text-slate-500 animate-pulse px-2">
-                Iniciando la base de datos de Neon (serverless). Esto puede tomar unos segundos...
+                Conectando...esto puede tomar unos segundos...
               </p>
             )}
           </div>
